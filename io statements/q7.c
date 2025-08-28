c086867f89326b6a2890cd576043adb1a3fe69ce
Raghuloffi <raghuls343@gmail.com>
2025-08-28 15:06:03 +0530

    Add files via upload

---
+++ io statements/q7.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("enter the hexadecimal");
+	scanf("%d",&a);
+	printf("%x",a);
+	return 0;
+}
---
+++ io statements/q8.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("enter the octal");
+	scanf("%d",&a);
+	printf("%o",a);
+	return 0;
+}
