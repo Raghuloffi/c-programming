33b7b29ae15a1d8dac6517f9650ef46f5045123c
Raghuloffi <raghuls343@gmail.com>
2025-09-04 14:20:34 +0530

    Add files via upload

---
+++ q11.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	char b;
+	printf("the char is=",b);
+	scanf("%c",&b);
+	printf("ASCII%d ",b);
+	return 0;
+}
---
+++ q12.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("enter ascii value");
+	scanf("%d",&a);
+	printf("enter the character %d %c",a,a);
+	return 0;
+}
