284b3eca6f76046f89b06d6521b151e2e2a8b160
Raghuloffi <raghuls343@gmail.com>
2025-09-11 14:25:08 +0530

    Add files via upload

---
+++ op 16.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("enter the number");
+	scanf("%d %d",&a,&b);
+	if(a==b)
+	{printf("this is true");
+	}else
+	{
+		printf("this is false");
+	}
+return 0;}
---
+++ op q15.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c,d;
+	printf("enter the number");
+	scanf("%d %d %d",&a,&b,&c);
+	d=(a+b+c)/3;
+	printf("the average of three number %d\n",d);
+	return 0;
+	
+}
